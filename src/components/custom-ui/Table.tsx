0cc78b61653549482d12b789eff0595e3585a79b
Himanshu Tamoli <himanshutamoli2005@gmail.com>
2025-08-29 12:34:17 +0530

    add initial components and styles; implement custom Table component with Tailwind CSS

---
+++ src/components/custom-ui/Table.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import {
+    Table as CustomTable,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '../ui/table'
+
+function Table() {
+    return (
+        <div>
+            <CustomTable>
+                <TableCaption>A list of your recent invoices.</TableCaption>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="w-[100px]">Invoice</TableHead>
+                        <TableHead>Status</TableHead>
+                        <TableHead>Method</TableHead>
+                        <TableHead className="text-right">Amount</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow>
+                        <TableCell className="font-medium">INV001</TableCell>
+                        <TableCell>Paid</TableCell>
+                        <TableCell>Credit Card</TableCell>
+                        <TableCell className="text-right">$250.00</TableCell>
+                    </TableRow>
+                </TableBody>
+            </CustomTable>
+        </div>
+    )
+}
+
+export default Table
