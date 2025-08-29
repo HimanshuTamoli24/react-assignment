8ad093ca0d07c2cdc116a8d1d4e99cfd47f64e3e
Himanshu Tamoli <himanshutamoli2005@gmail.com>
2025-08-29 20:28:33 +0530

    feat: add SideBar component and implement reusable UI components (Button, Dialog, Input, Select)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import Table from "@/components/custom-ui/Main"
+import SideBar from "@/components/custom-ui/SideBar";
 import { getProducts } from "@/utils/api/apis";
 import Image from "next/image";
 import { useEffect } from "react";
@@ -17,11 +18,48 @@ export default function Home() {
     fetchData();
   }, []);
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px]  items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <div className="border">
-        <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
-        <Table />
-      </div>
+    <div className="min-h-screen   ">
+
+      <Navbar />
+      <Table />
+
     </div>
+  );
+}
+import Link from "next/link";
+import { Github, LayoutDashboardIcon, Linkedin, ShoppingCart, User } from "lucide-react";
+
+function Navbar() {
+  return (
+    <nav className="w-full bg-white  rounded-2xl  sticky top-0 border  px-6 py-3 flex items-center justify-between">
+      {/* Logo / Brand */}
+      <div className="text-xl font-bold flex items-center gap-2 text-gray-800">
+
+        <LayoutDashboardIcon />
+        <span>React</span>
+
+      </div>
+
+      {/* Social Links */}
+      <div className="flex items-center space-x-4">
+        <Link
+          href="https://github.com/HimanshuTamoli24"
+          target="_blank"
+          className="flex items-center space-x-1 text-gray-700 hover:text-gray-900 font-medium"
+        >
+          <Github className="w-5 h-5" />
+
+        </Link>
+
+        <Link
+          href="https://www.linkedin.com/in/himanshutamoli24"
+          target="_blank"
+          className="flex items-center space-x-1 text-gray-700 hover:text-gray-900 font-medium"
+        >
+          <Linkedin className="w-5 h-5" />
+
+        </Link>
+      </div>
+    </nav>
   );
 }
