ea4e59fee70af445681b2597bf0bc61045859c19
Himanshu Tamoli <himanshutamoli2005@gmail.com>
2025-08-29 13:29:36 +0530

    add axios dependency and implement Table component for invoice display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,27 @@
-import Table from "@/components/custom-ui/Table";
+"use client"
+import Table from "@/components/custom-ui/Main"
+import { getProducts } from "@/utils/api/apis";
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await getProducts(5, 0);
+        console.log("Products:", data);
+      } catch (err) {
+        console.error("Error:", err);
+      }
+    }
+    fetchData();
+  }, []);
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px]  items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-     <div className="border">
-      <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
-      <Table/>
-     </div>
+      <div className="border">
+        <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
+        <Table />
+      </div>
     </div>
   );
 }
